6602b3bb94da3e6986178e55100b6c6a1e062526
Denis Zangarov <denisimo_98@yahoo.com>
2020-08-09 19:19:30 +0300

    Create 30 - Inserting a Row.sql

---
+++ Mosh Course/30 - Inserting a Row.sql
@@ -0,0 +1,19 @@
+USE sql_store;
+
+INSERT INTO customers(
+	last_name,
+    first_name,
+    birth_date,
+    address,
+    city,
+    state
+)
+VALUES (
+	"Smith",
+    "John",
+    "1990-01-01",
+    "address",
+    "city",
+    "CA",
+    DEFAULT
+)
