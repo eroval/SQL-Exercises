64ef786e7e01117a62523c1ed8d683d714e2025a
Denis Zangarov <denisimo_98@yahoo.com>
2020-08-09 19:22:44 +0300

    Create 53 - Numeric Functions.sql

---
+++ Mosh Course/53 - Numeric Functions.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(5.73) -- rounds to 6
+SELECT ROUND(5.73, 1) -- rounds to 1 after decimal
+SELECT TRUNCATE(5.7345, 2) -- keeps the 2 digits after decimal and removes the others
+SELECT CEILING(5.7) -- smallest integer that is GREATER than the number
+SELECT FLOOR(5.7) -- smallest integer that is LESSER than the number
+SELECT ABS(-4) -- absolute value
+SELECT RAND() -- randomized values
