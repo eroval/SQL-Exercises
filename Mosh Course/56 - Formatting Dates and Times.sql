b38256867ac5c8262738aed91b14aa3c2a122ce7
Denis Zangarov <denisimo_98@yahoo.com>
2020-08-19 19:33:29 +0300

    Create 56 - Formatting Dates and Times.sql

---
+++ Mosh Course/56 - Formatting Dates and Times.sql
@@ -0,0 +1,6 @@
+SELECT DATE_FORMAT(NOW(), "%m %y"); -- 2-digit year with month number
+SELECT DATE_FORMAT(NOW(), "%m %Y"); -- 4-digit year with month number
+SELECT DATE_FORMAT(NOW(), "%M %d %Y"); -- name of month with day and 4-digit year
+
+SELECT TIME_FORMAT(NOW(), "%H:%i %p") -- display time fomrated with : in a PM/AM state
+
