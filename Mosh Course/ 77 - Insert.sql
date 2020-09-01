24d19342c64b17c16116b3e02368ef45306ac8f4
Denis Zangarov <denisimo_98@yahoo.com>
2020-09-01 13:41:39 +0300

    Rename 77 - Insert.sql to  77 - Insert.sql

---
+++ Mosh Course/ 77 - Insert.sql
@@ -0,0 +1,4 @@
+USE sql_invoicing;
+
+INSERT INTO payments
+VALUES (DEFAULT, 5, 3, '2019-01-01', 10, 1)
--- Mosh Course/77 - Insert.sql
+++
@@ -1,4 +0,0 @@
-USE sql_invoicing;
-
-INSERT INTO payments
-VALUES (DEFAULT, 5, 3, '2019-01-01', 10, 1)
