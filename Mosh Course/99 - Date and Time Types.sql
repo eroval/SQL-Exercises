3344308713aa5edfcf5a43d182f27ac6d476a454
Denis Zangarov <denisimo_98@yahoo.com>
2020-09-01 13:27:31 +0300

    Create 99 - Date and Time Types.sql

---
+++ Mosh Course/99 - Date and Time Types.sql
@@ -0,0 +1,10 @@
+/*
+Date/Time types:
+
+- DATE      - format YYYY-MM-DD
+- TIME      - storing a time value
+- DATETIME  - 8b format: YYYY-MM-DD HH:MI:SS 
+(DATETIME should be used beyond 2038)
+- TIMESTAMP - 4b (up to 2038) YYYY-MM-DD HH:MI:SS
+- YEAR
+
