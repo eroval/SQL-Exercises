6ecb16b7b85dea170a3a57146b35ad0a674fc4a9
Denis Zangarov <denisimo_98@yahoo.com>
2020-09-06 14:25:22 +0300

    Create 132 - Storage Engines.sql

---
+++ Mosh Course/132 - Storage Engines.sql
@@ -0,0 +1,11 @@
+SHOW ENGINES
+
+/*
+ALTER TABLE customers
+ENGINE = InnoDB
+
+We can update/change current engine to a different one
+if we require some functionality which isn't handled by
+the current one.
+
+*/
